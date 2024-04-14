c7abb42e541d4bac0e041ed253daef788944e2ae
rafa1707 <rafa1707@gmail.com>
2024-04-14 11:39:57 -0300

    add update and delete mothods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get('flavour')
+    @Get()
     findAll() {
         return 'This action returns all coffees';
     }
@@ -15,5 +15,15 @@ export class CoffeesController {
     @Post()
     create(@Body() body) {
         return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `This action updates a #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} coffee`;
     }
 }
